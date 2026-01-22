c30273ae742e1d8ab6e4f48ae329f7136082c1ba
Arthur Adolfo <arthur.adolfo@sap.com>
2026-01-22 14:41:22 -0300

    feat: read variables from gradle.settings for project name

--- lua/gradle/parsers/settings_gradle_parser.lua
+++ lua/gradle/parsers/settings_gradle_parser.lua
@@ -10,6 +10,19 @@ local open = context_manager.open
 local SettingGradleParser = {}
 local project_name_pattern = "rootProject%.name%s*=%s*[%'" .. '%"' .. "](.-)[%'" .. '%"]'
 local module_pattern = "include%s*%(?[%'" .. '%"' .. "](.+)[%'" .. '%"]'
+local variable_pattern = "%${(%w+)}"
+
+local function read_variable_from_properties(variable_name)
+  local variable_value
+  with(open("gradle.properties"), function(reader)
+    for line in reader:lines() do
+      variable_value = string.match(line, variable_name .. "=(%w+)")
+      if variable_value then
+        return variable_value
+      end
+    end
+  end)
+end
 
 SettingGradleParser.parse_file = function(settings_gradle_path)
   local project_name
@@ -18,6 +31,12 @@ SettingGradleParser.parse_file = function(settings_gradle_path)
     for line in reader:lines() do
       if not project_name then
         project_name = string.match(line, project_name_pattern)
+        if project_name then
+          local variable_name = string.match(project_name, variable_pattern)
+          if variable_name then
+            project_name = read_variable_from_properties(variable_name)
+          end
+        end
       end
       for modules_match in string.gmatch(line, module_pattern) do
         modules_match = string.gsub(modules_match, "[%'" .. '%"%(%):]', '')
